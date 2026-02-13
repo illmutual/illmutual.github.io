ddd74791f5b74e4918cb22476ad38936e47d03eb
brooklyn <brooklynking@brooklyns-MacBook-Air-7.local>
2026-02-13 01:06:03 -0600

    initial astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ill-mutual.github.io',
 	integrations: [mdx(), sitemap()],
 });
+
+
